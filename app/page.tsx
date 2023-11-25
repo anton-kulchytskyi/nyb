5e2fae9a26161a8ee9d86f608566af415c309d4f
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-11-25 14:18:07 +0100

    added lint-staged and husky for autockeck before commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import FeauteredYachts from '@/components/FeauteredYachts/FeauteredYachts'
-import styles from './page.module.scss'
 import Header from '@/components/Header/Header'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
