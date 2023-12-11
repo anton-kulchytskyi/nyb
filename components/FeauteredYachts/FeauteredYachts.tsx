fda66a0bfe85f031532f682d0eca0717f180294c
Anton Kulchytskyi <antek1809@gmail.com>
2023-12-12 01:14:33 +0200

    First ver. adaptive design
    Also change some style in header section - make img to the whole height of viewport

--- components/FeauteredYachts/FeauteredYachts.tsx
+++ components/FeauteredYachts/FeauteredYachts.tsx
@@ -1,14 +1,22 @@
-import typo from '@/styles/typography.module.scss';
+// import dynamic from 'next/dynamic';
 import { Vessel } from '@/interfaces/vessel.interface';
 import { getFeauteredYacht } from '@/utils/api/getAllVessels';
-import FYCard from '../FYCard/FYCard';
+
+import typo from '@/styles/typography.module.scss';
+
 import img_1 from '../../public/fyc_1.jpeg';
 import img_2 from '../../public/fyc_2.jpeg';
 import img_3 from '../../public/fyc_3.jpeg';
+
+import FYCard from '../FYCard/FYCard';
 import TextButton from '../TextButton/TextButton';
+
 import styles from './feauteredYachts.module.scss';
 
-const imgs = [img_1, img_2, img_3]
+// const FYCard = dynamic(() => import('../FYCard/FYCard'), { ssr: false });
+
+const imgs = [img_1, img_2, img_3];
+const buttonsExample = ['Top 3', 'Hot Price', 'Low Price'];
 
 const FeauteredYachts = async () => {
   const yachts = await getFeauteredYacht();
@@ -23,11 +31,20 @@ const FeauteredYachts = async () => {
         <h5 className={typo.typo_h5}>yachts</h5>
       </div>
       <span className={styles.seeall}>
-        <TextButton text="See All" linkTo='/catalog' primary />
+        <TextButton
+          text="See All"
+          linkTo='/catalog'
+          primary
+        />
       </span>
       <div className={styles.cards_container}>
         {visibleYachts.map((yacht: Vessel, i: number) =>(
-          <FYCard key={yacht.vessel_id} yacht={yacht} photo={imgs[i]}/>
+          <FYCard
+            key={yacht.vessel_id}
+            yacht={yacht}
+            buttonsExample={buttonsExample[i]}
+            photo={imgs[i]}
+          />
         ))}
       </div>
     </section>
