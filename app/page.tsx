209c3681d2efe9070153519ab8b43e663fe07401
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2024-01-02 11:58:15 +0100

    Revert "Contact form"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Header from '@/components/Header/Header'
 import FeauteredYachts from '@/components/FeauteredYachts/FeauteredYachts'
-import ContactSection from '@/components/ContactSection/ContactSection'
 import ReviewsSection from '@/components/ReviewsSection/ReviewsSection'
-
 import styles from './page.module.scss'
 
 export default function Home() {
@@ -11,7 +9,6 @@ export default function Home() {
       <Header/>
       <main className={styles.main}>
         <FeauteredYachts />
-        <ContactSection />
         <ReviewsSection />
       </main>
     </>
