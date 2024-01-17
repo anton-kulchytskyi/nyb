cc32a32da5451896d10ea5cd2bbf80ba6c77ed5e
Anton Kulchytskyi <antek1809@gmail.com>
2024-01-17 17:57:37 +0200

    Added function for getting image from AWS
    Try to use environment variables on vercel deployment

--- components/FYCard/FYCard.tsx
+++ components/FYCard/FYCard.tsx
@@ -1,10 +1,15 @@
 'use client';
-import Image, { StaticImageData } from 'next/image';
+// import AWS from 'aws-sdk';
+import Image from 'next/image';
+// import Image, { StaticImageData } from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+// import { PromiseResult } from 'aws-sdk/lib/request';
 import { useCurrency } from '@/context/CurrencyContext';
 import { currencyArray } from '@/utils/currency/currencyArray';
+
+import { fetchImgUrl } from '@/utils/api/getImageFromAWS';
 
 import typo from '@/styles/typography.module.scss';
 import { Vessel } from '@/interfaces/vessel.interface';
@@ -16,14 +21,15 @@ import styles from './fycard.module.scss';
 interface Props {
   yacht: Vessel;
   buttonsExample?: string;
-  photo: StaticImageData;
+  // photo: StaticImageData;
   inCatalog?: boolean;
 }
 
-const FYCard = ({ yacht, buttonsExample, photo, inCatalog }: Props) => {
+const FYCard = ({ yacht, buttonsExample, inCatalog }: Props) => {
   const router = useRouter();
   const { selectedCurrency } = useCurrency();
   const [isHovering, setIsHovering] = useState(true);
+  const [imageUrl, setImageUrl] = useState<string>('');
   const {
     vessel_id,
     vessel_make,
@@ -31,6 +37,7 @@ const FYCard = ({ yacht, buttonsExample, photo, inCatalog }: Props) => {
     vessel_country,
     vessel_town,
     vessel_year,
+    vessel_image_key,
   } = yacht;
 
   const key = `vessel_price_${selectedCurrency}`;
@@ -40,6 +47,49 @@ const FYCard = ({ yacht, buttonsExample, photo, inCatalog }: Props) => {
   const currCurrency = currencyArray.find(
     (obj) => obj.currencyName === selectedCurrency
   )?.symbol;
+
+  useEffect(() => {
+    async function loadImgFromAws() {
+      const currImg = await fetchImgUrl(vessel_image_key);
+      setImageUrl(currImg);
+    }
+    loadImgFromAws();
+  }, [vessel_image_key]);
+
+  // useEffect(() => {
+  //   async function fetchImgUrl() {
+  //     try {
+  //       // Configure AWS SDK with the credentials and region
+  //       AWS.config.update({
+  //         accessKeyId: 'AKIAUXLH7DAVIEHB5FF2',
+  //         secretAccessKey: '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',
+  //         region: 'eu-north-1',
+  //       });
+  //       const s3 = new AWS.S3();
+  //       const bucketName = 'nyb-basket';
+  //       // Fetch the image from Amazon S3 based on the imageUrl from the good prop
+  //       const s3Object: PromiseResult<AWS.S3.GetObjectOutput, AWS.AWSError> =
+  //         await s3
+  //           .getObject({ Bucket: bucketName, Key: vessel_image_key })
+  //           .promise();
+  //       let currImageUrl;
+  //       if (s3Object.Body) {
+  //         currImageUrl = URL.createObjectURL(
+  //           new Blob([s3Object.Body as BlobPart])
+  //         );
+  //       } else {
+  //         // Handle the case where s3Object.Body is undefined
+  //         // eslint-disable-next-line
+  //         console.error('S3 object body is undefined');
+  //       }
+  //       setImageUrl(currImageUrl as string);
+  //     } catch (error) {
+  //       // eslint-disable-next-line
+  //       console.error('Error:', error);
+  //     }
+  //   }
+  //   fetchImgUrl();
+  // }, [vessel_image_key]);
 
   const handleMouseOver = () => {
     setIsHovering(false);
@@ -66,7 +116,7 @@ const FYCard = ({ yacht, buttonsExample, photo, inCatalog }: Props) => {
         }`}
       >
         <Image
-          src={photo}
+          src={imageUrl}
           fill
           sizes="100vw"
           className={styles.image}
---
+++ utils/api/getImageFromAWS.ts
@@ -0,0 +1,47 @@
+import AWS from 'aws-sdk';
+// import 'dotenv/config';
+// require('dotenv').config();
+import { PromiseResult } from 'aws-sdk/lib/request';
+// import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+
+export async function fetchImgUrl(keyFromAws: string): Promise<string> {
+  let currImageUrl;
+  try {
+    // Configure AWS SDK with the credentials and region
+    AWS.config.update({
+      // accessKeyId: 'AKIAUXLH7DAVIEHB5FF2',
+      // secretAccessKey: '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',
+      // region: 'eu-north-1',
+
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+
+      // accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+      // secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
+      // region: process.env.NEXT_PUBLIC_REGION,
+    });
+
+    const s3 = new AWS.S3();
+    const bucketName = 'nyb-basket';
+    // const bucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
+
+    // Fetch the image from Amazon S3 based on the imageUrl from the good prop
+    const s3Object: PromiseResult<AWS.S3.GetObjectOutput, AWS.AWSError> =
+      await s3.getObject({ Bucket: bucketName, Key: keyFromAws }).promise();
+
+    if (s3Object.Body) {
+      currImageUrl = URL.createObjectURL(new Blob([s3Object.Body as BlobPart]));
+    } else {
+      // Handle the case where s3Object.Body is undefined
+      // eslint-disable-next-line
+      console.error('S3 object body is undefined');
+    }
+    // setImageUrl(currImageUrl as string);
+  } catch (error) {
+    // eslint-disable-next-line
+    console.error('Error:', error);
+  }
+
+  return currImageUrl as string;
+}
