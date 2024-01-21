08fdbe17df1a7e9ffd92786d0333c6e1df9a54d7
Anton Kulchytskyi <antek1809@gmail.com>
2024-01-21 23:10:43 +0200

    sdk2 with env, and without NEXT_PUBLIC_ on vercel env

--- utils/api/getImageFromAWS.ts
+++ utils/api/getImageFromAWS.ts
@@ -5,33 +5,80 @@ console.log('secretAccessKey:', process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY);
 // eslint-disable-next-line
 console.log('region:', process.env.NEXT_PUBLIC_AWS_REGION);
 
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
 
-const client = new S3Client({
-  credentials: {
-    accessKeyId: 'AKIAUXLH7DAVIEHB5FF2',
-    secretAccessKey: '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',
-    // accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID as string,
-    // secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY as string,
-  },
-  // region: process.env.NEXT_PUBLIC_AWS_REGION as string,
-  region: 'eu-north-1',
-});
-
-export async function fetchImgUrl(keyFromAws: string) {
+export async function fetchImgUrl(keyFromAws: string): Promise<string> {
   let currImageUrl;
-  const command = new GetObjectCommand({
-    Bucket: 'nyb-basket',
-    Key: keyFromAws,
-  });
-
   try {
-    const response = await client.send(command);
-    const str = await response.Body?.transformToString('base64');
-    currImageUrl = `data:image/png;base64,${str}`;
-  } catch (err) {
+    // Configure AWS SDK with the credentials and region
+    AWS.config.update({
+      // accessKeyId: 'AKIAUXLH7DAVIEHB5FF2',
+      // secretAccessKey: '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',
+      // region: 'eu-north-1',
+
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+    });
+
+    const s3 = new AWS.S3();
+    const bucketName = 'nyb-basket';
+
+    // Fetch the image from Amazon S3 based on the imageUrl from the good prop
+    const s3Object: PromiseResult<AWS.S3.GetObjectOutput, AWS.AWSError> =
+      await s3.getObject({ Bucket: bucketName, Key: keyFromAws }).promise();
+
+    if (s3Object.Body) {
+      currImageUrl = URL.createObjectURL(new Blob([s3Object.Body as BlobPart]));
+    } else {
+      // Handle the case where s3Object.Body is undefined
+      // eslint-disable-next-line
+      console.error('S3 object body is undefined');
+    }
+    // setImageUrl(currImageUrl as string);
+  } catch (error) {
     // eslint-disable-next-line
-    console.error(err);
+    console.error('Error:', error);
   }
-  return currImageUrl as unknown as string;
+
+  // eslint-disable-next-line
+  console.log(currImageUrl);
+
+  return currImageUrl as string;
 }
+
+// import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+// const client = new S3Client({
+//   credentials: {
+//     accessKeyId: 'AKIAUXLH7DAVIEHB5FF2',
+//     secretAccessKey: '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',
+//     // accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID as string,
+//     // secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY as string,
+//   },
+//   // region: process.env.NEXT_PUBLIC_AWS_REGION as string,
+//   region: 'eu-north-1',
+// });
+
+// export async function fetchImgUrl(keyFromAws: string) {
+//   let currImageUrl;
+//   const command = new GetObjectCommand({
+//     Bucket: 'nyb-basket',
+//     Key: keyFromAws,
+//   });
+
+//   try {
+//     const response = await client.send(command);
+//     // const str = await response.Body?.transformToWebStream();
+//     const str = await response.Body?.transformToString('base64');
+//     // currImageUrl = URL.createObjectURL(new Blob([str as BlobPart]));
+//     currImageUrl = `data:image/png;base64,${str}`;
+//   } catch (err) {
+//     // eslint-disable-next-line
+//     console.error(err);
+//   }
+//   // eslint-disable-next-line
+//   console.log(currImageUrl);
+//   return currImageUrl as unknown as string;
+// }
