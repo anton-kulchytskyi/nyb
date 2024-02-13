2a1d06ad26eb7815d8b94b209967511b92dc3b83
Anton Kulchytskyi <antek1809@gmail.com>
2024-02-14 00:18:46 +0200

    moved slider settings to separete file

--- components/ReviewsSection/ReviewsSection.tsx
+++ components/ReviewsSection/ReviewsSection.tsx
@@ -8,6 +8,8 @@ import 'slick-carousel/slick/slick-theme.scss';
 import { reviewUser } from '@/interfaces/reviewsUsers.interface';
 import { users } from '@/utils/reviewsUsers/reviewsUsers';
 
+import { sliderSettings } from '@/utils/reviewsSlider/reviewsSliderSettings';
+
 const ReviewCardNoSSR = dynamic(
   () => import('@/components/ReviewsSection/ReviewsCard/ReviewCard'),
   { ssr: false }
@@ -17,53 +19,10 @@ import typo from '@/styles/typography.module.scss';
 import styles from './reviewsSection.module.scss';
 
 const ReviewsSection = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 2000,
-    cssEase: 'ease-in-out',
-    pauseOnHover: true,
-    swipeToSlide: true,
-    responsive: [
-      {
-        breakpoint: 1750,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-      {
-        breakpoint: 1410,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 1020,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 690,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
   return (
-    <section
-      // id="reviews"
-      className={styles.review_section}
-    >
+    <section className={styles.review_section}>
       <h3 className={`${typo.typo_h3} ${typo.typo_h3_gray}`}>Reviews</h3>
-      <Slider {...settings}>
+      <Slider {...sliderSettings}>
         {users.map((user: reviewUser) => (
           <ReviewCardNoSSR
             sliderIdx={user.sliderIdx}
