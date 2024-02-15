8f4908f66215af1d34da9484ae041a82aed2b819
Anton Kulchytskyi <antek1809@gmail.com>
2024-02-15 15:40:32 +0200

    replaced slick with swiper
    removed autoplay, added arrows

--- components/ReviewsSection/ReviewsSection.tsx
+++ components/ReviewsSection/ReviewsSection.tsx
@@ -1,14 +1,13 @@
 'use client';
 import dynamic from 'next/dynamic';
 
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.scss';
-import 'slick-carousel/slick/slick-theme.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 import { reviewUser } from '@/interfaces/reviewsUsers.interface';
 import { users } from '@/utils/reviewsUsers/reviewsUsers';
-
-import { sliderSettings } from '@/utils/reviewsSlider/reviewsSliderSettings';
 
 const ReviewCardNoSSR = dynamic(
   () => import('@/components/ReviewsSection/ReviewsCard/ReviewCard'),
@@ -22,19 +21,40 @@ const ReviewsSection = () => {
   return (
     <section className={styles.review_section}>
       <h3 className={`${typo.typo_h3} ${typo.typo_h3_gray}`}>Reviews</h3>
-      <Slider {...sliderSettings}>
+      <Swiper
+        navigation={true}
+        loop={true}
+        modules={[Navigation]}
+        slidesPerView={1}
+        breakpoints={{
+          700: {
+            slidesPerView: 2,
+          },
+          1020: {
+            slidesPerView: 3,
+          },
+          1340: {
+            slidesPerView: 4,
+          },
+          1760: {
+            slidesPerView: 5,
+          },
+        }}
+        className="reviewsSwiper"
+      >
         {users.map((user: reviewUser) => (
-          <ReviewCardNoSSR
-            sliderIdx={user.sliderIdx}
-            key={user.userId}
-            userName={user.userName}
-            userAvatar={user.userAvatar}
-            date={user.date}
-            stars={user.stars}
-            reviewText={user.reviewText}
-          />
+          <SwiperSlide key={user.userId}>
+            <ReviewCardNoSSR
+              sliderIdx={user.sliderIdx}
+              userName={user.userName}
+              userAvatar={user.userAvatar}
+              date={user.date}
+              stars={user.stars}
+              reviewText={user.reviewText}
+            />
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </section>
   );
 };
