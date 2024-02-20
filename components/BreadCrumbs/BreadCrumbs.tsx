d8ea4f738825eb9c1bfb7e6ce00c190f68c09504
Anton Kulchytskyi <antek1809@gmail.com>
2024-02-20 15:12:37 +0200

    fixed breadcrumbs at 404 page
    change file structure with adding group router (pages)
    
    minir style changes on 404 page
    
    new file and logic for links

--- components/BreadCrumbs/BreadCrumbs.tsx
+++ components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
 
 import React from 'react';
 import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation';
 import HomePageSvg from '../SvgIconsComponents/HomePageSvg';
 import RightArrowSvg from '../SvgIconsComponents/RightArrowSvg';
 import styles from './BreadCrumbs.module.scss';
@@ -11,35 +11,47 @@ const BreadCrumbs = () => {
   const paths = usePathname();
   const search = useSearchParams();
   const yachtName = search.get('name');
-  const pathsWithoutDashes = paths.split('/').slice(1).map(path => path.split('-').join(' ')).slice(0, 1);
+  const pathsWithoutDashes = paths
+    .split('/')
+    .slice(1)
+    .map((path) => path.split('-').join(' '))
+    .slice(0, 1);
   if (yachtName) {
     pathsWithoutDashes.push(yachtName);
   }
 
   return (
-    pathsWithoutDashes[0] !== '' &&
-      <div className={styles.body}>
-        <Link href="/">
-          <div className={styles.kek}>
-            <HomePageSvg />
-          </div>
-        </Link>
-        <RightArrowSvg color={false} />
-        {pathsWithoutDashes.map((path, index) => {
-          const link = `/${pathsWithoutDashes.map(p => p.split(' ').join('-')).slice(0, index + 1).join('/')}`
-          return (
-            <React.Fragment key={index}>
-              <Link
-                className={pathsWithoutDashes.length - 1 === index ? styles.disabled : styles.item}
-                href={link}
-              >
-                {path}
-              </Link>
-              {pathsWithoutDashes.length !== index + 1 && <RightArrowSvg color={false} />}
-            </React.Fragment>
-          )
-        })}
-      </div>
+    <div className={styles.body}>
+      <Link href="/">
+        <div className={styles.kek}>
+          <HomePageSvg />
+        </div>
+      </Link>
+      <RightArrowSvg color={false} />
+      {pathsWithoutDashes.map((path, index) => {
+        const link = `/${pathsWithoutDashes
+          .map((p) => p.split(' ').join('-'))
+          .slice(0, index + 1)
+          .join('/')}`;
+        return (
+          <React.Fragment key={index}>
+            <Link
+              className={
+                pathsWithoutDashes.length - 1 === index
+                  ? styles.disabled
+                  : styles.item
+              }
+              href={link}
+            >
+              {path}
+            </Link>
+            {pathsWithoutDashes.length !== index + 1 && (
+              <RightArrowSvg color={false} />
+            )}
+          </React.Fragment>
+        );
+      })}
+    </div>
   );
 };
 
