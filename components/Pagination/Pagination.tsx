1b0bef804590c00887a906a15aa5b402ea4cffc9
Anton Kulchytskyi <antek1809@gmail.com>
2024-02-25 00:32:58 +0200

    changed pagination design
    TO DO: check behavior with more pages

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 import styles from './pagination.module.scss';
 
 type Props = {
   items: number;
   pageSize: number;
   currentPage: number;
-}
+};
 
 const Pagination = ({ items, pageSize, currentPage }: Props) => {
   const pagesCount = Math.ceil(items / pageSize);
@@ -13,23 +13,58 @@ const Pagination = ({ items, pageSize, currentPage }: Props) => {
   if (pagesCount === 1) return null;
 
   const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
-  
-  return(
+
+  return (
     <>
       <ul className={styles.pagination}>
-        <li>
-          <Link 
-            className={currentPage === 1 ? styles.disabled : ''}
-            href={`?page=${currentPage  === 1 ? currentPage : currentPage - 1}&size=${pageSize}`}>&lt;</Link>
+        <li className={currentPage === 1 ? styles.disabled : ''}>
+          <Link
+            href={`?page=${
+              currentPage === 1 ? currentPage : currentPage - 1
+            }&size=${pageSize}`}
+          >
+            &#10094;
+          </Link>
         </li>
 
         {pages.map((page) => {
           const isFirstOrLastPage = page === 1 || page === pages.length;
-          const isInMiddle = (currentPage === page - 3 || page + 3 === currentPage) ;
-          const isOutOfRange = (page > currentPage && currentPage <= (page - 3)) || (page < currentPage && currentPage >= (page + 3));
+          const isInMiddle =
+            currentPage === page - 3 || page + 3 === currentPage;
+          const isOutOfRange =
+            (page > currentPage && currentPage <= page - 3) ||
+            (page < currentPage && currentPage >= page + 3);
 
-          if(isFirstOrLastPage){
-            return <li
+          if (isFirstOrLastPage) {
+            return (
+              <li
+                key={page}
+                className={
+                  page === currentPage ? styles.pageItemActive : styles.pageItem
+                }
+              >
+                <Link href={`?page=${page}&size=${pageSize}`}>{page}</Link>
+              </li>
+            );
+          }
+
+          if (isInMiddle) {
+            return (
+              <li
+                key={page}
+                className={styles.middle}
+              >
+                ...
+              </li>
+            );
+          }
+
+          if (isOutOfRange) {
+            return;
+          }
+
+          return (
+            <li
               key={page}
               className={
                 page === currentPage ? styles.pageItemActive : styles.pageItem
@@ -37,34 +72,21 @@ const Pagination = ({ items, pageSize, currentPage }: Props) => {
             >
               <Link href={`?page=${page}&size=${pageSize}`}>{page}</Link>
             </li>
-          }
-
-          if (isInMiddle) {
-            return <li key={page}>....</li>;
-          }
-
-          if(isOutOfRange) {
-            return;
-          }
-
-          return <li
-            key={page}
-            className={
-              page === currentPage ? styles.pageItemActive : styles.pageItem
-            }
-          >
-            <Link href={`?page=${page}&size=${pageSize}`}>{page}</Link>
-          </li>
+          );
         })}
 
-        <li>
-          <Link 
-            className={currentPage === pagesCount ? styles.disabled : ''}
-            href={`?page=${currentPage  === pagesCount ? currentPage : currentPage + 1}&size=${pageSize}`}>&gt;</Link>
+        <li className={currentPage === pagesCount ? styles.disabled : ''}>
+          <Link
+            href={`?page=${
+              currentPage === pagesCount ? currentPage : currentPage + 1
+            }&size=${pageSize}`}
+          >
+            &#10095;
+          </Link>
         </li>
       </ul>
     </>
-  )
+  );
 };
 
 export default Pagination;
