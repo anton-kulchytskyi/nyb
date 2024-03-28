ac50a4a8207f16d906ef2a4104f64d84b255f494
Anton Kulchytskyi <antek1809@gmail.com>
2024-03-28 12:51:08 +0200

    Added skeleton for product card gallery

--- components/ProductCard/ProductCardGallery/ProductCardGallerySkeleton.tsx
+++ components/ProductCard/ProductCardGallery/ProductCardGallerySkeleton.tsx
@@ -6,34 +6,24 @@ const ProductCardGallerySkeleton = () => {
       <div className={styles.gallery__container}>
         <div className={styles.row}>
           <div className={styles.triple_col}>
-            <h2>Slider skeleton here</h2>
+            <div className={styles.gallery__slider}>
+              <div className={styles.loadingSkeleton}>
+                <div className={styles.loading_image}></div>
+              </div>
+            </div>
           </div>
           <div className={styles.col}>
             <div className={styles.gallery__side}>
-              <h4 className={styles.gallery__side__title}>Bla</h4>
-              <h4 className={styles.gallery__side__title}>Bla</h4>
-              <span className={styles.gallery__side__place}>Bla</span>
-              <div className={styles.gallery__side__info}>
-                <p className={styles.line}>
-                  <span className={styles.line__name}>Price:</span>
-                  <span className={styles.line__price}>446646</span>
-                </p>
-                <p className={styles.line}>
-                  <span className={styles.line__name}>Year:</span>
-                  <span className={styles.line__value}>20606</span>
-                </p>
-                <p className={styles.line}>
-                  <span className={styles.line__name}>Cabin:</span>
-                  <span className={styles.line__value}>4</span>
-                </p>
-                <p className={styles.line}>
-                  <span className={styles.line__name}>Berth:</span>
-                  <span className={styles.line__value}>3</span>
-                </p>
-                <p className={styles.line}>
-                  <span className={styles.line__name}>Toilets:</span>
-                  <span className={styles.line__value}>2</span>
-                </p>
+              <div className={styles.loadingSkeleton}>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
+                <div className={styles.loading_text}></div>
               </div>
             </div>
           </div>
